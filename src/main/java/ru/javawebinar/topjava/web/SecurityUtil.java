63046c1aab841662a90f8c339a24018f0684bd30
zepno <ggalaev@mail.ru>
2023-02-14 20:55:46 +0400

    add logged for user

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,11 +4,17 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
+    private static int userId = 1;
+
     public static int authUserId() {
-        return 1;
+        return userId;
     }
 
     public static int authUserCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
+    }
+
+    public static void setAuthUserId(int userId) {
+        SecurityUtil.userId = userId;
     }
 }
