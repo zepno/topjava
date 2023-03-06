21f872370f4248ee1173f1d3030fd85732f84fd6
zepno <ggalaev@mail.ru>
2023-03-06 19:38:16 +0400

    implemented DataJpaMealRepository

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -33,7 +33,6 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-@Ignore
 public class MealServiceTest {
     private static final Logger log = getLogger("result");
 
