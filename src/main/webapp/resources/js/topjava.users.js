660ceec27b7dc5812b4e20243b4da3506632ed71
zepno <ggalaev@mail.ru>
2023-04-04 18:55:39 +0400

    8_09_ajax_datatables

---
+++ src/main/webapp/resources/js/topjava.users.js
@@ -0,0 +1,42 @@
+﻿const ajaxUrl = "admin/users/";
+let datatableApi;
+
+// $(document).ready(function () {
+$(function () {
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "name"
+            },
+            {
+                "data": "email"
+            },
+            {
+                "data": "roles"
+            },
+            {
+                "data": "enabled"
+            },
+            {
+                "data": "registered"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "asc"
+            ]
+        ]
+    });
+    makeEditable();
+});
